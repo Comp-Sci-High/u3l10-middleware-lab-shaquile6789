1dd113a0eff851283028cee913458cc4fbf1f131
shaquile6789 <shaquille.oemrawsingh26@compscihigh.org>
2026-01-12 21:35:05 +0000

    finished

--- index.js
+++ index.js
@@ -9,6 +9,8 @@
 //    (Use dot notation & string concatenation)
 // -7 sends back a 404 page for all other paths
 
+const express = require('express')
+const app = express ()
 // PINK Only: Add the correct status codes to ALL route handlers
 
 const animalShelterData = {
@@ -22,3 +24,36 @@ const animalShelterData = {
         { name: "Cozy Paws Sanctuary", location: "456 Oak Avenue, Townsburg" }
     ]
 };
+
+app.use((req,res, next) =>{
+    console.log(req.method, req.url)
+    next()
+})
+
+app.get("/", (req,res) =>{
+    res.status(200).send('<h1>Welcome to the Animal Shelter Network</h1>')
+})
+
+app.get("api/cat", (req, res) => {
+    res.status(200).json(animalShelterData.cats)
+})
+
+app.get("api/shelters", (req, res) => {
+    res.status(200).json(animalShelterData.shelters)
+})
+
+app.get("docs", (req, res) => {
+    res.status(200).send('Go to /api/cat to see cats for adoption and /api/shelters to see shelters in the area')
+})
+
+app.get("adopt/cat", (req, res) => {
+    res.status(200).send('A cat you can adopt is ' + animalShelterData.cats[0].name)
+})
+
+app.get((req, res) => {
+    res.status(404).send('404 Page Not Found')
+})
+
+app.listen(3000, () => {
+    console.log('Server is running')
+})
